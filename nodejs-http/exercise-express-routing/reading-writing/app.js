7d31a8dc853ec08cda4f1e3fc7f43d099f738f2a
melinaskye <melinaskye@optonline.net>
2024-02-01 15:41:49 -0500

    week 2

--- nodejs-http/exercise-express-routing/reading-writing/app.js
+++ nodejs-http/exercise-express-routing/reading-writing/app.js
@@ -1,3 +1,42 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// let bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+
+// let products = [];
+
+// app.post('/products', function(req, res) {
+//   const newProduct = { ...req.body, id: products.length + 1 }
+//   products = [ ...products, newProduct]
+//   res.json(newProduct);
+// });
+
+// app.put('/products', function(req, res) {
+//   let updatedProduct;
+//   products = products.map(p => {
+//     if (p.id === req.body.id) {
+//       updatedProduct = { ...p, ...req.body };
+//       return updatedProduct;
+//     }
+//     return p;
+//   })
+//   res.json(updatedProduct);
+// });
+
+// app.delete('/products/:id', function(req, res) {
+//   const deletedProduct = products.find(p => p.id === +req.params.id);
+//   products = products.filter(p => p.id !== +req.params.id);
+//   res.json(deletedProduct);
+// });
+
+// app.get('/products', (req, res) => {
+//   res.json(products);
+// })
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,21 +46,30 @@ app.use(bodyParser.json());
 
 let products = [];
 
-app.post('/products', function (req, res) {
-  // implement
-});
+app.route('/products')
+ .get((req, res) => {
+   res.json(products);
+ })
+ .post((req, res) => {
+   const newProduct = { ...req.body, id: products.length + 1 }
+   products = [...products, newProduct]
+   res.json(newProduct);
+ })
+.put((req, res) => {
+   let updatedProduct;
+   products = products.map(p => {
+     if (p.id === req.body.id) {
+       updatedProduct = { ...p, ...req.body };
+       return updatedProduct;
+     }
+     return p;
+   })
+   res.json(updatedProduct);
+ })
+ .delete((req, res) => {
+   const deletedProduct = products.find(p => p.id === +req.body.id);
+   products = products.filter(p => p.id !== +req.body.id);
+   res.json(deletedProduct);
+ })
 
-app.put('/products', function (req, res) {
-  // implement
-});
-
-app.delete('/products/:id', function (req, res) {
-  // implement
-});
-
-app.get('/products', (req, res) => {
-  // implement
-})
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-  
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
